683926faaf4d30277a8607035d86e04c6d0268b2
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2019-06-25 14:41:05 -0500

    Update server.js

--- arrasio/src/server/server.js
+++ arrasio/src/server/server.js
@@ -2507,7 +2507,8 @@ class Entity {
                 (this.team !== -2 && room.isIn('bas2', loc)) ||
                 (this.team !== -3 && room.isIn('bas3', loc)) ||
                 (this.team !== -4 && room.isIn('bas4', loc))
-            ) { this.kill(); }
+            ) { //this.kill();
+	    	      this.health.amount -= 1;}
         }
     }
 
